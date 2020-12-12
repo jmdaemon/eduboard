44fbf8f86fd51c66b2ffbc78920484a3b96229ca
Joseph Diza <josephm.diza@gmail.com>
2020-12-12 14:56:18 -0800

    Clean up UserAccount.kt

--- app/src/main/java/com/github/jmd/user-account/UserAccount.kt
+++ app/src/main/java/com/github/jmd/user-account/UserAccount.kt
@@ -1,7 +1,5 @@
 package com.github.jmd.user
 
-
-//class User (firstName: String, lastName: String, userName: String, password: String) {
 class User (firstName: String, lastName: String, userName: Int, password: Int) { 
 
   var firstName: String = ""
@@ -10,66 +8,4 @@ class User (firstName: String, lastName: String, userName: Int, password: Int) {
   var password: Int = 0
     get() = field
     set(value) { field = value }
-
-  //constructor (
-  //var firstName: String = firstName, 
-  //var lastName: String = lastName, 
-  //var userName: Int = userName, 
-  //var password: Int = password,
-      //get() = field
-      //set(value) { field = value }
-    //private set
-  //get() = field
-
-  //get() = field
-  //set(value) { field = value }
-
-  //get getFirstName(): String {
-    //return firstName
-  //}
-  //get getLastName(): String {
-    //return lastName
-  //}
-
-  //get getUserName(): Int {
-    //return userName
-  //}
-
-  //get getPassword(): Int {
-    //return password 
-  //}
-
-    ////get() = this.firstName
-
-  //val getLastName: String 
-    //get() = this.lastName
-
-  //val getUserName: Int
-  //get() = this.userName
-
-  //val getPassword: Int
-  //get() = this.password
-
-
-    //val userName: String,
-    //val password: String,
-    //val firstName: Int,
-    //val lastName: Int,
-
-    //fun getFname() : String {
-      //return firstName
-    //}
-
-    //fun getLname() : String {
-      //return lastName
-    //}
-
-    //fun getUserName() : Int {
-      //return userName
-    //}
-
-    //fun getPassword() : Int {
-      //return password 
-    //}
-    
 }
