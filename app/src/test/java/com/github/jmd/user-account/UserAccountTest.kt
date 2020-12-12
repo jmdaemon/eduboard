7a090c91dba36a108661e862ff85daf402595566
Joseph Diza <josephm.diza@gmail.com>
2020-12-12 14:55:43 -0800

    Remove dead code and imports in UserAccountTest

--- app/src/test/java/com/github/jmd/user-account/UserAccountTest.kt
+++ app/src/test/java/com/github/jmd/user-account/UserAccountTest.kt
@@ -1,21 +1,12 @@
 package com.github.jmd.user
 
-//import kotlin-test-junit5
-
-//import org.Junit.Test
-//import org.junit.Assert.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class UserAccountTests { 
   @Test 
   fun User_WhenInit_ReturnsUser() { 
-    //val user = User("Paul", "Allen", 112233, 123456); 
     val user = User("Paul", "Allen", 112233, 123456); 
-    //assertEquals("Paul", user.getFirstName())
-    //assertEquals("Allen", user.getListName())
-    //assertEquals(112233, user.getUserName())
-    //assertEquals(123456, user.getPassword())
     assertEquals("Paul", user.firstName)
     assertEquals("Allen", user.lastName)
     assertEquals(112233, user.userName)
