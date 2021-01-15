845e272c8f9df0bacfa35bd12cfdf60115921eec
Joseph Diza <josephm.diza@gmail.com>
2021-01-15 14:28:36 -0800

    Fix bug where UserAccount did not set mem vars

--- app/src/main/java/com/github/jmd/user-account/UserAccount.kt
+++ app/src/main/java/com/github/jmd/user-account/UserAccount.kt
@@ -1,11 +1,22 @@
 package com.github.jmd.user
 
-class User (firstName: String, lastName: String, userName: Int, password: Int) { 
+//class User (firstName: String, lastName: String, userName: Int, password: Int) { 
+class User (fName: String, lName: String, uName: Int, pass: Int) { 
 
-  var firstName: String = ""
-  var lastName: String = ""
-  var userName: Int = 0
-  var password: Int = 0
-    get() = field
-    set(value) { field = value }
+  var firstName: String
+  var lastName: String
+  var userName: Int
+  var password: Int
+
+  init { 
+    //firstName = firstName
+    //lastName = lastName
+    //userName = userName
+    //password = password
+
+    firstName = fName
+    lastName = lName
+    userName = uName
+    password = pass
+  }
 }
