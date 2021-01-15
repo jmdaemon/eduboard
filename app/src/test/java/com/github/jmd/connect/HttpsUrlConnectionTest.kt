3beb3273b8f23cdbf80daf37f5d475f72ff1954e
Joseph Diza <josephm.diza@gmail.com>
2021-01-15 14:18:42 -0800

    Remove dead code in HttpsUrlConnectionTest and remove old dependencies

--- app/src/test/java/com/github/jmd/connect/HttpsUrlConnectionTest.kt
+++ app/src/test/java/com/github/jmd/connect/HttpsUrlConnectionTest.kt
@@ -5,18 +5,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-//import kotlin.test.junit5
-//import kotlin.test.Test
-//import kotlin.test.assertTrue
-//import kotlin.test.assertEquals
-//import kotlin.test.assertNotNull
-
 class HttpsUrlConnectionTests {
   val Http = Http()
 
   @Test
   fun runReturnsResponse() {
-    println(Http.run())
     assertNotNull(Http.run())
   }
 }
