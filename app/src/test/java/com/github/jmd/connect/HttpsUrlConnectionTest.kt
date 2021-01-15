d8e88624340f19a9724c8aac56cb270940b26b1a
Joseph Diza <josephm.diza@gmail.com>
2021-01-15 14:17:33 -0800

    Detect and run tests in build.gradle

--- app/src/test/java/com/github/jmd/connect/HttpsUrlConnectionTest.kt
+++ app/src/test/java/com/github/jmd/connect/HttpsUrlConnectionTest.kt
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+//import kotlin.test.junit5
+//import kotlin.test.Test
+//import kotlin.test.assertTrue
+//import kotlin.test.assertEquals
+//import kotlin.test.assertNotNull
+
 class HttpsUrlConnectionTests {
   val Http = Http()
 
